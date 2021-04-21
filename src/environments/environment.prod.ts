7ca6fd4199b8b87be76e8f776263d46a0b8b24ef
Ariz Hernández <arishassam@gmail.com>
2021-04-20 23:48:05 -0600

    github-pages ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDXSliCVu0kdTvyqVsaBLmff35e_vWOBi8",
+    authDomain: "ingreso-egreso-app-ar.firebaseapp.com",
+    projectId: "ingreso-egreso-app-ar",
+    storageBucket: "ingreso-egreso-app-ar.appspot.com",
+    messagingSenderId: "576469265669",
+    appId: "1:576469265669:web:9ec3d29d93c2e28cb09b6c",
+    measurementId: "G-4960022FZB"
+  }
 };
